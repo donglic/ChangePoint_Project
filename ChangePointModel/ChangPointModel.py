68a75541a6fadf397c968abd6e0a8acce57205af
Dongli Chen <chendo@yorku.ca>
2025-09-17 20:46:06 -0400

    Adding the place-holder for the change point model implementation in Python

---
+++ ChangePointModel/ChangPointModel.py
@@ -0,0 +1,6 @@
+"""
+This is the python file that contains the implementation of the Chang Point Model. It is currently blank.
+More Documentation will be added along with the project development
+
+"""
+
